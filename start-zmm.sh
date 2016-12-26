72225d8f21bafdfc3e4c74dd5a730eb77993fb9a
Volodymyr Molin <vmolin@lohika.com>
2016-12-26 18:06:12 +0200

    Run Mesos and Marathon in background

--- start-zmm.sh
+++ start-zmm.sh
@@ -10,17 +10,17 @@
 # Run Mesos Master       #
 ##########################
 
-/usr/sbin/mesos-master --zk=zk://localhost:2181/mesos --quorum=1 --work_dir=/usr/lib/mesos/master
+/usr/sbin/mesos-master --zk=zk://localhost:2181/mesos --quorum=1 --work_dir=/usr/lib/mesos/master &
 
 ##########################
 # Run Mesos Agent        #
 ##########################
 
-/usr/sbin/mesos-slave --no-systemd_enable_support  --master=zk://localhost:2181/mesos --work_dir=/usr/lib/mesos/agent
+/usr/sbin/mesos-slave --no-systemd_enable_support  --master=zk://localhost:2181/mesos --work_dir=/usr/lib/mesos/agent &
 
 ##########################
 # Run Marathon           #
 ##########################
 
 export MESOS_NATIVE_JAVA_LIBRARY=/usr/lib/libmesos.so
-/root/marathon-1.3.5/bin/start --master zk://localhost:2181/mesos --zk zk://localhost:2181/marathon
+/root/marathon-1.3.5/bin/start --master zk://localhost:2181/mesos --zk zk://localhost:2181/marathon &
