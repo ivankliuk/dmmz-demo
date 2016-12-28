18eae89d3a6a8272fec908124f1935782fcb6e20
Ivan Kliuk <ivan.kliuk@gmail.com>
2016-12-28 15:39:49 +0200

    Add export command

--- start_dockerd.sh
+++ start_dockerd.sh
@@ -9,8 +9,8 @@ echo "# Starting Docker Daemon #"
 echo "##########################"
 echo
 
-LD_LIBRARY_PATH=$CHROOT_PATH/lib:$CHROOT_PATH/lib64:$CHROOT_PATH/usr/lib:$CHROOT_PATH/usr/lib64:/lib:/lib64:/usr/lib:/usr/lib64
-PATH=$CHROOT_PATH/bin:$CHROOT_PATH/sbin:$CHROOT_PATH/usr/bin:$CHROOT_PATH/usr/sbin
+export LD_LIBRARY_PATH=$CHROOT_PATH/lib:$CHROOT_PATH/lib64:$CHROOT_PATH/usr/lib:$CHROOT_PATH/usr/lib64:/lib:/lib64:/usr/lib:/usr/lib64
+export PATH=$CHROOT_PATH/bin:$CHROOT_PATH/sbin:$CHROOT_PATH/usr/bin:$CHROOT_PATH/usr/sbin
 
 $CHROOT_PATH/bin/docker daemon -D \
  --exec-root=$CHROOT_PATH/var/run/docker \
